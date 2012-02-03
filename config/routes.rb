2b58ae8c9bcf636b167019f8cebbe37aa38aff49
Ruben Nodeland <ruben.nodeland@gmail.com>
2012-02-03 15:56:07 +0100

    Added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  root :to => "pages#home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
